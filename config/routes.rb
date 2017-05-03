10eea87e028edaf43669170e9f37e773e57c7910
funnythingz <funnythingz69+github@gmail.com>
2017-05-03 16:33:38 +0900

    added community

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/community', to: 'community#view', as: 'community'
+  put '/community', to: 'community#put', as: 'put_community'
+
 end
